f514d2de7e38371e4e703f934b915068162e38f6
nitish3000 <hardwaj37@gmail.com>
2026-05-22 15:09:08 +0530

    Modified 1_Setup_Data.sql : created the three tables for later queries

--- 1_Setup_Data.sql
+++ 1_Setup_Data.sql
@@ -1,13 +1,78 @@
--- WEEK 3 :
+-- WEEK 3 : The Dataset of superstore_raw tells us about the details of orders' transactions in each row
+
 -- Tasks:
+
+-- STEP 1: Setting up Data
+
 CREATE DATABASE IF NOT EXISTS `superstore_analysis`;
 USE `superstore_analysis`;
--- hello
- CREATE TABLE customers (
-    'Customer ID' VARCHAR(50) PRIMARY KEY,
-    'Customer Name' VARCHAR(50),
-    'Segment' VARCHAR(50),
-   )
+-- import the superstore_raw table into this dataset
 
- INSERT INTO customers ( 'Customer ID', 'Customer Name', 'Segment')
- SELECT DISTINCT `Customer 
+CREATE TABLE  IF NOT EXISTS customers (
+    Customer_ID VARCHAR(50) PRIMARY KEY,
+    Customer_Name VARCHAR(50),
+    Segment VARCHAR(50) 
+    );
+INSERT IGNORE INTO customers (Customer_ID, Customer_Name, segment)
+SELECT DISTINCT `Customer ID`, `Customer Name`, `Segment`
+FROM `superstore_raw`;
+SELECT * FROM customers LIMIT 5 ;
+
+CREATE TABLE IF NOT EXISTS orders ( 
+`Row ID` INT PRIMARY KEY,
+Order_ID VARCHAR(50),
+Order_Date DATE,
+Ship_Date DATE,
+Ship_Mode VARCHAR(50),
+Customer_ID VARCHAR(50),
+Country VARCHAR(50),
+City VARCHAR(50),
+State VARCHAR(50),
+Postal_Code VARCHAR(20),
+Region VARCHAR(50),
+Product_ID VARCHAR(50),
+Sales DECIMAL(10, 2),
+Quantity INT,
+Discount DECIMAL(4, 2),
+Profit DECIMAL(10, 2)
+) ;
+INSERT IGNORE INTO orders (`Row ID`, Order_ID, Order_Date, Ship_Date, Ship_Mode, Customer_ID, Country, City, State, Postal_Code, Region, Product_ID, Sales, Quantity, Discount, Profit)
+SELECT DISTINCT 
+    `Row ID`, 
+    `Order ID`, 
+    STR_TO_DATE(`Order Date`, '%m/%d/%Y'), 
+    STR_TO_DATE(`Ship Date`, '%m/%d/%Y'),  
+    `Ship Mode`, 
+    `Customer ID`, 
+    `Country`, 
+    `City`, 
+    `State`, 
+    `Postal Code`, 
+    `Region`, 
+    `Product ID`, 
+    `Sales`, 
+    `Quantity`, 
+    `Discount`, 
+    `Profit`
+FROM `superstore_raw` ;
+SELECT * FROM orders LIMIT 10 ;
+
+CREATE TABLE IF NOT EXISTS products (
+    Row_ID INT PRIMARY KEY,
+    Product_ID VARCHAR(50),
+    Category VARCHAR(50),
+    Sub_Category VARCHAR(50),
+    Product_Name VARCHAR(255)
+);
+INSERT IGNORE INTO products ( Row_ID, Product_ID, Category, Sub_Category, Product_Name )
+SELECT DISTINCT
+`Row ID`,
+`Product ID`,
+`Category`,
+`Sub-Category`,
+`Product Name`
+FROM superstore_raw ;
+SELECT * FROM products LIMIT 5 ;
+
+
+ 
