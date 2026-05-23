dbbebdb029193f139d533357a60b58ce59fafebc
nitish3000 <hardwaj37@gmail.com>
2026-05-24 00:20:21 +0530

    Performed all the required queries mentioned in step 2 of Tasks

--- 2_Performing_Queries.sql
+++ 2_Performing_Queries.sql
@@ -19,4 +19,57 @@ WHERE ( Customer_ID, Sales ) IN (
 
 -- 3. Calculate total sales for each customer. (CTE)
 -- in CTE we basically make a temporary virtual table for our further analysis
-SELECT Customer_ID, Sales
+-- CTE always start with WITH
+WITH CustomerSalesCTE AS (
+	SELECT Customer_ID, SUM(Sales) AS Total_Customer_Sales
+    FROM orders
+    GROUP BY Customer_ID
+    )
+SELECT Customer_ID, Total_Customer_Sales 
+FROM CustomerSalesCTE ;
+    
+-- 4. Find customers whose total sales are above average. (CTE + Subquery)    
+WITH Customer_SalesCTE AS (
+	SELECT Customer_ID, SUM(Sales) AS Total_Customer_Sales 
+    FROM orders
+    GROUP BY Customer_ID
+    )
+SELECT Customer_ID, Total_Customer_Sales
+FROM Customer_SalesCTE
+WHERE Total_Customer_Sales > (
+	SELECT  AVG(TOTAL_Customer_Sales) 
+    FROM Customer_SalesCTE
+    );
+
+-- 5. Rank all customers based on total sales. (Window Function)
+SELECT
+	Customer_ID,
+    SUM(Sales) AS Total_Sales,
+    RANK() OVER ( ORDER BY SUM(Sales) DESC ) AS Rank_of_Customers   
+FROM orders
+GROUP BY Customer_ID
+LIMIT 5 ;    -- for short proof
+
+-- 6. Assign row numbers to each order within a customer. (Window Function + PARTITION BY)		
+SELECT 
+    Customer_ID, 
+    Order_ID, 
+    Order_Date, 
+    Sales,
+    ROW_NUMBER() OVER(PARTITION BY Customer_ID ORDER BY Order_Date ASC) AS Order_Sequence_Number
+FROM orders;
+
+-- 7. Display top 3 customers based on total sales. (Window Function)
+SELECT Customer_ID, Total_Sales, Customer_Rank
+FROM (
+    SELECT 
+        Customer_ID, 
+        SUM(Sales) AS Total_Sales,
+        DENSE_RANK() OVER (ORDER BY SUM(Sales) DESC) AS Customer_Rank
+    FROM orders
+    GROUP BY Customer_ID
+) AS RankedCustomers
+WHERE Customer_Rank <= 3;
+
+
+
